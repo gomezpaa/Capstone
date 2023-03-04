29381c6b60f43dbcaee4851a2a3df709e71f380c
gomezpaa <gomez.paolo@outlook.com>
2023-03-04 00:21:41 -0500

    Added a new connection to the db.js file. It will connect to the NYT database and query the database to display articles on NYT.ejs.

--- db.js
+++ db.js
@@ -1,7 +1,7 @@
 const mysql = require('mysql');
 require('dotenv').config()
 
-const connection = mysql.createConnection({
+const testConnection = mysql.createConnection({
   host: '127.0.0.1',
   port: 3306,
   user: process.env.user,
@@ -9,14 +9,35 @@ const connection = mysql.createConnection({
   database: 'test'
 });
 
-connection.connect((err) => {
-  if (err) throw err;
-  console.log("Connected to the database!");
+const nytConnection = mysql.createConnection({
+  host: '127.0.0.1',
+  port: 3306,
+  user: process.env.user,
+  password: process.env.password,
+  database: 'NYT'
 });
 
-exports.getData = function(callback) {
-  connection.query('SELECT * FROM nyt_us', (err, results) => {
+testConnection.connect((err) => {
+  if (err) throw err;
+  console.log("Connected to the test database!");
+});
+
+exports.getData = (function(callback) {
+  testConnection.query('SELECT * FROM nyt_us', (err, results) => {
     if (err) throw err;
     callback(results);
   });
-};
+});
+
+nytConnection.connect((err) => {
+  if (err) throw err;
+  console.log("Connected to the NYT database!");
+});
+
+exports.getFullData = (function(callback) {
+  nytConnection.query('SELECT news_title, news_description, news_link, article_text FROM news_items JOIN news_article ON news_items.article_id = news_article.article_id GROUP BY news_items.article_id', (err, results) =>{
+    if(err) throw err;
+    callback(results);
+  });
+});
+
